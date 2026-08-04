be8c9aa778a02cafdc09bf0c6751eb90b201da66
Ammarahfarooqi <ammarahfarooqi6@gmail.com>
2026-08-04 16:56:01 +0500

    Add search improvements and delete confirmation

--- inventory.py
+++ inventory.py
@@ -4,6 +4,7 @@ class Inventory:
     def __init__(self):
         self.products = []
 
+
     def add_product(self):
         try:
             prod_id = int(input("Enter Product ID : "))
@@ -24,6 +25,7 @@ class Inventory:
         print("\nProduct added successfully!\n")
         return True
 
+
     def display_products(self):
         if len(self.products) == 0:
             print("\nNo products available\n")
@@ -31,17 +33,39 @@ class Inventory:
             for item in self.products:
                 item.display()
 
-    def search_product(self): 
-        product_id = int(input("Enter Product ID : "))
-        found = False
-        for item in self.products:
-            if product_id == item.prod_id:
-                found = True
-                print("\nProduct found successfully!\n")
-                item.display()
-                break
-        if found == False:
-            print("\nProduct not found.\n")
+
+    def search_product(self):
+        while True:
+            try :
+                search = int(input("1. Search by ID OR 2. Search by name\nEnter your choice : ")) 
+            except ValueError:
+                print("\nEnter a valid choice\n")
+                continue
+            if search == 1:
+                product_id = int(input("\nEnter Product ID : "))
+                found = False
+                for item in self.products:
+                    if product_id == item.prod_id:
+                        found = True
+                        print("\nProduct found successfully!\n")
+                        item.display()
+                        break
+                if found == False:
+                    print("\nProduct not found.\n")
+            elif search == 2:
+                product_name = input("\nEnter Product Name : ")
+                found = False
+                for item in self.products:
+                    if product_name.lower() == item.name.lower():
+                        found = True
+                        print("\nProduct found successfully!\n")
+                        item.display()
+                        break
+                if found == False:
+                    print("\nProduct not found.\n")
+            else:
+                print("\nEnter a valid no\n")
+
 
     def delete_product(self):
         product_id = int(input("Enter Product ID : "))
@@ -49,11 +73,20 @@ class Inventory:
         for item in self.products:
             if product_id == item.prod_id:
                 found = True
-                self.products.remove(item)
-                print("\nProduct deleted successfully!\n")
-                break
+                while True:
+                    confirm = input("Are you sure want to delete? (Y/N) : ")
+                    if confirm == 'y' or confirm == 'Y':
+                        self.products.remove(item)
+                        print("\nProduct deleted successfully!\n")
+                        break
+                    elif confirm == 'n' or confirm == 'N':
+                        print("\nDeletion cancelled.\n")
+                        break
+                    else:
+                        print("\nEnter valid choice\n")
         if found == False:
             print("\nProduct not found.\n")
+
 
     def update_product(self):
         product_id = int(input("Enter Product ID : "))
@@ -71,6 +104,8 @@ class Inventory:
         if found == False:
             print("\nProduct not found.\n")
 
+
+
     def inventory_summary(self):
         total_products = len(self.products)
         total_stock = 0
@@ -80,6 +115,7 @@ class Inventory:
             total_value += item.stock * item.price
         print("========== INVENTORY SUMMARY ==========\n")
         print("Total Products : ", total_products, "\nTotal Stock : ", total_stock, "\nInventory Value : ", total_value)
+
 
     def load_products(self):
         try:
@@ -92,6 +128,7 @@ class Inventory:
         except FileNotFoundError:
             print("\nNo saved products found. Starting with an empty inventory.\n")
             return 
+
 
     def save_products(self):
         with open("products.txt", "w") as f:
